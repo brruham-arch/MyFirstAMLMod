3a2e56d883737466c8ad24f88bb69af4268e8e8b
Burhan <apasaja@gmail.com>
2026-03-09 06:51:23 +0700

    test: write to internal data dir

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <mod/amlmod.h>
+#include <mod/logger.h>
 #include <android/log.h>
 #include <stdio.h>
 
 MYMOD(com.burhan.myfirstmod, MyFirstMod, 1.0, Burhan)
 
-// Ini dipanggil saat .so di-dlopen, sebelum apapun
+static Logger loggerLocal;
+Logger* logger = &loggerLocal;
+
 __attribute__((constructor))
 static void onDlOpen()
 {
-    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== dlopen! ===");
-    FILE* f = fopen("/storage/emulated/0/Android/data/com.sampmobilerp.game/modtest.txt", "w");
-    if(f) { fprintf(f, "dlopen OK\n"); fclose(f); }
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== CONSTRUCTOR FIRED ===");
+    // Tulis ke internal app data (pasti bisa ditulis)
+    FILE* f = fopen("/data/data/com.sampmobilerp.game/modtest.txt", "w");
+    if(f) { fprintf(f, "constructor OK\n"); fclose(f); }
 }
 
-ON_MOD_PRELOAD() {
-    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== PRELOAD! ===");
+ON_MOD_PRELOAD()
+{
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== PRELOAD ===");
 }
 
-ON_MOD_LOAD() {
-    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== LOAD! ===");
+ON_MOD_LOAD()
+{
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== LOAD ===");
 }
