bb0225d45ae5fe2de5d9db9f302e62bc84d16a87
Burhan <apasaja@gmail.com>
2026-03-09 01:00:58 +0700

    test: preload + tulis ke sdcard root

--- main.cpp
+++ main.cpp
@@ -8,14 +8,16 @@ MYMOD(com.burhan.myfirstmod, MyFirstMod, 1.0, Burhan)
 static Logger loggerLocal;
 Logger* logger = &loggerLocal;
 
+ON_MOD_PRELOAD()
+{
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== PRELOAD dipanggil! ===");
+    FILE* f = fopen("/sdcard/modtest.txt", "w");
+    if(f) { fprintf(f, "PRELOAD OK\n"); fclose(f); }
+}
+
 ON_MOD_LOAD()
 {
-    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "OnModLoad called! aml=%p", aml);
-
-    FILE* f = fopen("/storage/emulated/0/Download/modtest.txt", "w");
-    if(f)
-    {
-        fprintf(f, "OnModLoad dipanggil!\naml pointer: %p\n", aml);
-        fclose(f);
-    }
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== LOAD dipanggil! ===");
+    FILE* f = fopen("/sdcard/modtest.txt", "a");
+    if(f) { fprintf(f, "LOAD OK\naml=%p\n", aml); fclose(f); }
 }
