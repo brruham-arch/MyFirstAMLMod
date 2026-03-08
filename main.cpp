0add8983a3108d97746b58f0830351e8dbebdddf
Burhan <apasaja@gmail.com>
2026-03-09 01:06:23 +0700

    test: write to game data dir

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ Logger* logger = &loggerLocal;
 
 ON_MOD_PRELOAD()
 {
-    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== PRELOAD dipanggil! ===");
-    FILE* f = fopen("/sdcard/modtest.txt", "w");
-    if(f) { fprintf(f, "PRELOAD OK\n"); fclose(f); }
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "PRELOAD! aml=%p", aml);
+    FILE* f = fopen("/storage/emulated/0/Android/data/com.sampmobilerp.game/modtest.txt", "w");
+    if(f) { fprintf(f, "PRELOAD\naml=%p\n", aml); fclose(f); }
 }
 
 ON_MOD_LOAD()
 {
-    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== LOAD dipanggil! ===");
-    FILE* f = fopen("/sdcard/modtest.txt", "a");
-    if(f) { fprintf(f, "LOAD OK\naml=%p\n", aml); fclose(f); }
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "LOAD! aml=%p", aml);
+    FILE* f = fopen("/storage/emulated/0/Android/data/com.sampmobilerp.game/modtest.txt", "a");
+    if(f) { fprintf(f, "LOAD\naml=%p\n", aml); fclose(f); }
 }
