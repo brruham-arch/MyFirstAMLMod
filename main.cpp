0acb11d964ecfecf9923081d5c0905487462a9c2
Burhan <apasaja@gmail.com>
2026-03-09 00:35:47 +0700

    test: raw android log

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <mod/amlmod.h>
 #include <mod/logger.h>
+#include <android/log.h>
 
 MYMOD(com.burhan.myfirstmod, MyFirstMod, 1.0, Burhan)
+
 static Logger loggerLocal;
 Logger* logger = &loggerLocal;
+
 ON_MOD_LOAD()
 {
-aml->ShowToast(true, "MyFirstMod loaded!");
-    logger->SetTag("MyFirstMod");
-    logger->Info("=== MyFirstMod berhasil di-load! ===");
-
-    uintptr_t pGTASA = aml->GetLib("libGTASA.so");
-    if(pGTASA)
-        logger->Info("libGTASA.so ditemukan di: 0x%X", pGTASA);
-    else
-        logger->Error("libGTASA.so tidak ditemukan!");
+    __android_log_print(ANDROID_LOG_ERROR, "MYFIRSTMOD", "=== OnModLoad dipanggil! aml=%p ===", aml);
+    
+    if(aml)
+    {
+        aml->ShowToast(true, "MyFirstMod loaded!");
+        logger->SetTag("MyFirstMod");
+        logger->Info("=== berhasil! ===");
+    }
 }
