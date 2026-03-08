901689c379e18e0952ed975f654e8195e7fd3ddd
Burhan <apasaja@gmail.com>
2026-03-08 18:28:01 +0700

    fix OnModLoad use JNIEXPORT macro

--- main.cpp
+++ main.cpp
@@ -3,18 +3,14 @@
 
 MYMOD(com.burhan.myfirstmod, MyFirstMod, 1.0, Burhan)
 
-extern "C" void OnModLoad()
+ON_MOD_LOAD()
 {
     logger->SetTag("MyFirstMod");
     logger->Info("=== MyFirstMod berhasil di-load! ===");
 
     uintptr_t pGTASA = aml->GetLib("libGTASA.so");
     if(pGTASA)
-    {
         logger->Info("libGTASA.so ditemukan di: 0x%X", pGTASA);
-    }
     else
-    {
         logger->Error("libGTASA.so tidak ditemukan!");
-    }
 }
