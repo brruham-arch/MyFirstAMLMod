c5549218bc0d9cd207905dc7eaa0e7826a95919d
Burhan <apasaja@gmail.com>
2026-03-09 07:31:17 +0700

    v0.3 fix ModInfo redefinition

--- main.cpp
+++ main.cpp
@@ -4,24 +4,13 @@
 
 #define LOG(fmt, ...) __android_log_print(ANDROID_LOG_INFO, "MyFirstMod", fmt, ##__VA_ARGS__)
 
-struct ModInfo {
-    char szGUID[48];
-    char szName[48];
-    char szVersion[24];
-    char szAuthor[48];
-};
-
-static ModInfo g_modinfo = {
-    "com.burhan.myfirstmod",
-    "MyFirstMod",
-    "1.0",
-    "Burhan"
-};
+static ModInfo g_modinfo("com.burhan.myfirstmod", "MyFirstMod", "1.0", "Burhan");
+ModInfo* modinfo = &g_modinfo;
 
 IAML* aml = nullptr;
 
 extern "C" __attribute__((visibility("default"))) ModInfo* __GetModInfo() {
-    return &g_modinfo;
+    return modinfo;
 }
 
 extern "C" __attribute__((visibility("default"))) void OnModLoad() {
