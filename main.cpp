b0cb87a9cd8777709928cfcde10da8c865e1f722
Burhan <apasaja@gmail.com>
2026-03-09 21:12:10 +0700

    AntiPause only via nm offset

--- main.cpp
+++ main.cpp
@@ -8,44 +8,42 @@ static ModInfo g_modinfo("com.burhan.myfirstmod", "AntiPause", "1.0", "Burhan");
 ModInfo* modinfo = &g_modinfo;
 IAML* aml = nullptr;
 
-typedef void (*AndroidPaused_t)();
-AndroidPaused_t origAndroidPaused = nullptr;
-void HookedAndroidPaused() {
-    LOG("BLOCK: AndroidPaused()");
-}
-
-typedef void (*SetAndroidPaused_t)(int);
+typedef void (*SetAndroidPaused_t)(int paused);
 SetAndroidPaused_t origSetAndroidPaused = nullptr;
-void HookedSetAndroidPaused(int paused) {
-    LOG("SetAndroidPaused(%d)", paused);
-    if(paused) return;
+
+void HookedSetAndroidPaused(int paused)
+{
+    if(paused) {
+        LOG("AntiPause: block SetAndroidPaused!");
+        return;
+    }
     origSetAndroidPaused(paused);
 }
 
 typedef void (*AndroidPause_t)();
 AndroidPause_t origAndroidPause = nullptr;
-void HookedAndroidPause() {
-    LOG("BLOCK: AndroidPause()");
+
+void HookedAndroidPause()
+{
+    LOG("AntiPause: block AndroidPause!");
 }
 
 extern "C" __attribute__((visibility("default"))) ModInfo* __GetModInfo() { return modinfo; }
 
 extern "C" __attribute__((visibility("default"))) void OnModLoad() {
     aml = (IAML*)GetInterface("AMLInterface");
-    if(!aml) return;
 
     uintptr_t pGTASA = aml->GetLib("libGTASA.so");
     LOG("libGTASA = 0x%X", pGTASA);
 
-    aml->Hook((void*)(pGTASA + 0x269ad4), (void*)HookedAndroidPaused,
-              (void**)&origAndroidPaused);
-    LOG("AndroidPaused hooked!");
+    uintptr_t fnSetPaused = pGTASA + 0x00269ae4;
+    uintptr_t fnAndPause  = pGTASA + 0x00269af4;
 
-    aml->Hook((void*)(pGTASA + 0x269ae4), (void*)HookedSetAndroidPaused,
+    aml->Hook((void*)fnSetPaused, (void*)HookedSetAndroidPaused,
               (void**)&origSetAndroidPaused);
     LOG("SetAndroidPaused hooked!");
 
-    aml->Hook((void*)(pGTASA + 0x269af4), (void*)HookedAndroidPause,
+    aml->Hook((void*)fnAndPause, (void*)HookedAndroidPause,
               (void**)&origAndroidPause);
     LOG("AndroidPause hooked!");
 
