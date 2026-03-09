ab45397b9897ddc03afbc15c15c175bcd0ac4308
Burhan <apasaja@gmail.com>
2026-03-09 12:03:06 +0700

    v0.9 hook RakNet Receive

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <mod/amlmod.h>
 #include <mod/iaml.h>
 #include <android/log.h>
-#include <string.h>
 
 #define LOG(fmt, ...) __android_log_print(ANDROID_LOG_INFO, "MyFirstMod", fmt, ##__VA_ARGS__)
 
@@ -9,18 +8,24 @@ static ModInfo g_modinfo("com.burhan.myfirstmod", "MyFirstMod", "1.0", "Burhan")
 ModInfo* modinfo = &g_modinfo;
 IAML* aml = nullptr;
 
-void dumpBytes(const char* label, uintptr_t addr, int count) {
-    char buf[256] = {0};
-    char tmp[8];
-    for(int i = 0; i < count; i++) {
-        snprintf(tmp, sizeof(tmp), "%02X ", *(unsigned char*)(addr + i));
-        strcat(buf, tmp);
-        if((i+1) % 16 == 0) {
-            LOG("%s+0x%X: %s", label, i-15, buf);
-            buf[0] = 0;
+// Sama seperti onReceivePacket di Lua
+typedef bool (*ReceivePacket_t)(void* rakpeer, unsigned char* data, int len, void* addr);
+ReceivePacket_t origReceivePacket = nullptr;
+
+bool HookedReceivePacket(void* rakpeer, unsigned char* data, int len, void* addr)
+{
+    if(data && len > 0)
+    {
+        unsigned char packetID = data[0];
+        
+        // Block sync packet dari player lain
+        if(packetID == 207 || packetID == 208 || 
+           packetID == 209 || packetID == 203)
+        {
+            return true; // drop
         }
     }
-    if(buf[0]) LOG("%s: %s", label, buf);
+    return origReceivePacket(rakpeer, data, len, addr);
 }
 
 extern "C" __attribute__((visibility("default"))) ModInfo* __GetModInfo() {
@@ -31,16 +36,23 @@ extern "C" __attribute__((visibility("default"))) void OnModLoad() {
     aml = (IAML*)GetInterface("AMLInterface");
     if(!aml) return;
 
-    uintptr_t pSAMP = aml->GetLib("libsamp.so");
-    uintptr_t pORIG = aml->GetLib("libSAMP_ORIG.so");
-
-    // Dump sekitar offset yang ketemu
-    uintptr_t addr1 = pSAMP + 0xDD0D0;
-    uintptr_t addr2 = pORIG + 0x19092E;
-
-    LOG("=== libsamp.so+0xDD0D0 ===");
-    dumpBytes("samp", addr1 - 32, 64); // dump sebelum dan sesudah
-
-    LOG("=== libSAMP_ORIG.so+0x19092E ===");
-    dumpBytes("orig", addr2 - 32, 64);
+    // Hook RakNet::Receive di libsamp.so
+    // Pattern: fungsi yang menerima packet dari network
+    uintptr_t fnReceive = aml->PatternScan(
+        "2D E9 F0 4F ?? ?? ?? ?? ?? ?? ?? ?? 04 46",
+        "libsamp.so"
+    );
+    
+    LOG("RakNet Receive = 0x%X", fnReceive);
+    
+    if(fnReceive)
+    {
+        aml->Hook(fnReceive, (void*)HookedReceivePacket, (void**)&origReceivePacket);
+        LOG("Hook terpasang!");
+        aml->ShowToast(true, "SyncBlocker aktif!");
+    }
+    else
+    {
+        LOG("Fungsi tidak ditemukan, perlu cari pattern lain");
+    }
 }
