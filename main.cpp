31fb46598a5f2b97071deff3000a7537da62b2a5
Burhan <apasaja@gmail.com>
2026-03-10 12:39:25 +0700

    core mod base

--- main.cpp
+++ main.cpp
@@ -1,13 +1,45 @@
 #include <mod/amlmod.h>
 #include <mod/iaml.h>
 #include <android/log.h>
+#include <cstdint>
 
-#define LOG(fmt, ...) __android_log_print(ANDROID_LOG_INFO, "BurhanAML", fmt, ##__VA_ARGS__)
+#define LOG_TAG "BurhanCore"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-static ModInfo g_modinfo("com.burhan.testmod", "BurhanTestMod", "1.0", "Burhan");
+static ModInfo g_modinfo("com.burhan.coremod", "BurhanCoreMod", "1.0", "Burhanudin");
 ModInfo* modinfo = &g_modinfo;
 
 IAML* aml = nullptr;
+
+uintptr_t libGTASA = 0;
+uintptr_t libSAMP = 0;
+
+void DetectLibraries()
+{
+    libGTASA = (uintptr_t)aml->GetLib("libGTASA.so");
+    libSAMP  = (uintptr_t)aml->GetLib("libsamp.so");
+
+    if(libGTASA)
+        LOGI("libGTASA base: %p", (void*)libGTASA);
+    else
+        LOGE("libGTASA not found");
+
+    if(libSAMP)
+        LOGI("libsamp base: %p", (void*)libSAMP);
+}
+
+void InitMod()
+{
+    LOGI("InitMod start");
+
+    DetectLibraries();
+
+    if(libGTASA)
+        aml->ShowToast(true, "GTASA detected");
+
+    LOGI("InitMod complete");
+}
 
 extern "C"
 __attribute__((visibility("default")))
@@ -20,16 +52,19 @@ extern "C"
 __attribute__((visibility("default")))
 void OnModLoad()
 {
-    LOG("=== BurhanTestMod loaded ===");
+    LOGI("=== BurhanCoreMod loaded ===");
 
     aml = (IAML*)GetInterface("AMLInterface");
+
     if(!aml)
     {
-        LOG("AML interface not found");
+        LOGE("AML interface not found");
         return;
     }
 
-    LOG("AML interface acquired");
+    LOGI("AML interface acquired");
 
-    aml->ShowToast(true, "BurhanTestMod aktif");
+    aml->ShowToast(true, "BurhanCoreMod aktif");
+
+    InitMod();
 }
